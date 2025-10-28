411756c2bae511569bb9ccc1918d203f2339cc2f
Alyssa Gramling <alyssagramling55@gmail.com>
2025-10-28 11:27:52 -0400

    completed with tests passing

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 import express from "express";
 const app = express();
 export default app;
+
+import foldersRouter from "#api/folders-router";
+import filesRouter from "#api/files-router";
+
+app.use(express.json());
+
+app.use("/folders", foldersRouter);
+app.use("/files", filesRouter);
+
+app.use((err, req, res, next) => {
+  if (err.code === "23505") {
+    return res.status(400).send(err.detail);
+  }
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.send(500).send("Sorry! Something went wrong.");
+});
